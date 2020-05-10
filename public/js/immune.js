e931d87bf22859d747feafa926b181b80794cbd9
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2020-05-10 12:28:48 +1000

    jquery change

--- public/js/immune.js
+++ public/js/immune.js
@@ -1,3 +1,4 @@
+jQuery.noConflict()(function ($) {
 $(function(){
     $("#sideMenu a").bind("click", function(){
         $("#sideMenu a").removeClass("clicked"); // Remove all highlights
@@ -6,7 +7,7 @@ $(function(){
 });
 
 //Home highlighted when the PatientDashbaord page is loaded
-jQuery.noConflict()(function ($) {
+
 $(document).ready(function() {
     $("#sideMenu a").slice(0,1,2,3,4,5,6).css({"background": "rgba(228, 241, 254, 1)", "border-left": "5px solid rgba(44, 62, 80, 1)",
     "padding": "15px 15px 15px 25px", "width": "280px", "transition": "0s"});
