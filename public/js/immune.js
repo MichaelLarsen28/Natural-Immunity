755699b60a8236ec96601da4e4d7f550548995f9
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2020-05-10 12:37:50 +1000

    jquery change

--- public/js/immune.js
+++ public/js/immune.js
@@ -1,4 +1,4 @@
-jQuery.noConflict()(function ($) {
+(function( $ ) {
 $(function(){
     $("#sideMenu a").bind("click", function(){
         $("#sideMenu a").removeClass("clicked"); // Remove all highlights
@@ -13,4 +13,4 @@ $(document).ready(function() {
     "padding": "15px 15px 15px 25px", "width": "280px", "transition": "0s"});
     $("#sideMenu a i").slice(0,1,2,3,4,5,6).css({"color": "rgba(44, 62, 80, 1)"});
 });
-});
+})(jQuery);
