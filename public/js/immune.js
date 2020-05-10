cce07e03d8153e355d3f88b3aa4a9490b48ee32e
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2020-05-10 21:06:21 +1000

    new design

--- public/js/immune.js
+++ public/js/immune.js
@@ -1,4 +1,4 @@
-/*(function( $ ) {
+(function( $ ) {
 $(function(){
     $("#sideMenu a").bind("click", function(){
         $("#sideMenu a").removeClass("clicked"); // Remove all highlights
@@ -7,11 +7,9 @@ $(function(){
 });
 
 //Home highlighted when the PatientDashbaord page is loaded
-
+/*
 $(document).ready(function() {
-    $("#sideMenu a").slice(0,1,2,3,4,5,6).css({"background": "white", "border-left": "5px solid rgba(44, 62, 80, 1)",
+    $("#sideMenu a").slice(0,1,2,3,4,5,6).css({"background": "white", "border-bottom": "3px solid rgba(82, 179, 217, 1)",
     "padding": "1.2%", "width": "80%", "transition": "0s"});
-    $("#sideMenu a i").slice(0,1,2,3,4,5,6).css({"color": "rgba(44, 62, 80, 1)"});
-});
+});*/
 })(jQuery);
-*/
